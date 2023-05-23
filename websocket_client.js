60e7fd63838f3d41e1148544a16bd18ac0d54111
Taehwa Kang <tkang@Taehwaui-MacBookPro.local>
2023-05-23 22:11:35 +0900

    Chap2 example code

--- websocket_client.js
+++ websocket_client.js
@@ -1,56 +1,54 @@
-var socket = WebSocket("ws://echo.websocket.org");
-
-socket.onopen = function() {
-  console.log("Connection established");
-
-  let label = document.getElementById("status-label");
-  label.innerHTML = "Connection established";
-}
-
-socket.onmessage = function(event) {
-  if (typeof event.data === "string") {
-    let label = document.getElementById("status-label");
-    label.innerHTML = event.data;
-  }
-}
-
-socket.onclose = function(event) {
-  console.log("Connection closed");
-
-  let code = event.code;
-  let reason = event.reason;
-  let wasClean = event.wasClean;
-
+window.onload = function() {
+  let socket = new WebSocket("wss://socketsbay.com/wss/v2/1/demo/");
+  let textView = document.getElementById("text-view");
+  let buttonSend = document.getElementById("send-button");
+  let buttonStop = document.getElementById("stop-button");
   let label = document.getElementById("status-label");
 
-  if (wasClean) {
-    label.innerHTML = "Connection closed normally";
-  } else {
-    label.innerHTML = "Connection closed with message: " + reason + " (Code: " + code + ")";
+  socket.onopen = function() {
+    console.log("Connection established");
+
+    label.innerHTML = "Connection established";
   }
-}
 
-socket.onerror = function(event) {
-  console.log("Error occurred.");
-
-  let label = document.getElementById("status-label");
-  label.innerHTML = "Error : " + event;
-}
-
-let textView = document.getElementById("text-view");
-let buttonSend = document.getElementById("send-button");
-let buttonStop = document.getElementById("stop-button");
-
-buttonSend.onclick = function() {
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.send(textView.value);
-    textView.value = "";
+  socket.onmessage = function(event) {
+    if (typeof event.data === "string") {
+      label.innerHTML = event.data;
+    }
   }
-}
 
-buttonStop.onclick = function() {
-  if (socket.readyState === WebSocket.OPEN) {
-    socket.close(1000, "Deliberate disconnection");
+  socket.onclose = function(event) {
+    console.log("Connection closed");
+
+    let code = event.code;
+    let reason = event.reason;
+    let wasClean = event.wasClean;
+
+    if (wasClean) {
+      label.innerHTML = "Connection closed normally";
+    } else {
+      label.innerHTML = "Connection closed with message: " + reason + " (Code: " + code + ")";
+    }
+  }
+
+  socket.onerror = function(event) {
+    console.log("Error occurred.");
+
+    label.innerHTML = "Error : " + event;
+  }
+
+
+  buttonSend.onclick = function() {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(textView.value);
+      textView.value = "";
+    }
+  }
+
+  buttonStop.onclick = function() {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.close(1000, "Deliberate disconnection");
+    }
   }
 }
 
